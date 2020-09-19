084635850032659d69bfc1faf660cfefaafc87cf
Ankit Joshi <ankitjoshicr7@gmail.com>
2020-09-19 11:45:39 +0530

    Added LoginForm using AuthenticationForm

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,7 @@
 from blog.models import Blog , Article
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
 
 class BlogForm (forms.ModelForm):
     class Meta:
@@ -10,3 +12,7 @@ class ArticleForm( forms.ModelForm):
     class Meta:
         model = Article
         fields  = ['title' , 'body' , 'draft']
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label = "Username" , max_length = 30 , widget = forms.TextInput(attrs = {'name': 'username'}))
+    password = forms.Charfield( label = "Password" , max_length = 30 , widget = forms.PasswordInput(attrs = {'name': 'password'}))
